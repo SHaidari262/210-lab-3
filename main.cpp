0e3dabd12fde14c1ace0248c9c4659496d976b10
Sorya Dawood <soryadawood@Soryas-MacBook.local>
2026-09-02 12:06:28 -0700

    Writing the function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// Restaurant struct
 struct Restaurant {
     string name;            // Name of the restaurant
     int noOfTables;         // Numbwer of tables in the restaurant
@@ -8,3 +9,28 @@ struct Restaurant {
     string phoneNumber;     // Phone number of the restaurant
     double rating;          // Rating of the restaurant
 };
+
+// Function to create struct instance
+Restaurant createRestaurant ()
+{
+    // Create a Restaurant struct object
+    Restaurant r;
+
+    // Get the name
+    cout << "Enter the name of the restaurant: ";
+    getline(cin, r.name);
+
+    // Get the number of tables
+    cout << "Enter the number of tables: ";
+    cin >> r.noOfTables;
+    cin.ignore(1000, 10); // Ignore the newline character left in the input buffer
+
+    // Return the Restaurant struct object
+    return r;
+}
+
+// main fuction
+int main() {
+    // Create an instance of the Restaurant struct
+
+}
