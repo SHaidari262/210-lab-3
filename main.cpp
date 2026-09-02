8de8898ca21aec053af3fb88b35a0430db806848
Sorya Dawood <soryadawood@Soryas-MacBook.local>
2026-09-02 11:43:34 -0700

    Added attributes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 struct Restaurant {
-
-
+    string name;            // Name of the restaurant
+    int noOfTables;         // Numbwer of tables in the restaurant
+    string address;         // Address of the restaurant
+    string phoneNumber;     // Phone number of the restaurant
+    double rating;          // Rating of the restaurant
 };
